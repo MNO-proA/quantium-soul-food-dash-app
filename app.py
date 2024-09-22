0fc6a4d7477fad7bf1873d7bff01549089e3fa96
mno-proa <michaelnanaofosuofficial@gmail.com>
2024-09-22 14:10:32 +0000

    refactored code

--- app.py
+++ app.py
@@ -10,18 +10,21 @@ sales_data = pd.read_csv(data_file_path)
 # Convert 'date' to datetime format for proper sorting and filtering
 sales_data['date'] = pd.to_datetime(sales_data['date'])
 
+# sort data by date
+sales_data = sales_data.sort_values('date')
+
 # Create a Dash app
 app = dash.Dash(__name__)
 
 # Create a line chart using Plotly Express
-fig = px.line(sales_data, x='date', y='sales', title='Sales Over Time',
+line_chart = px.line(sales_data, x='date', y='sales', title='Sales Over Time',
               labels={'sales': 'Sales ($)', 'date': 'Date'})
 
 # Convert the price increase date to datetime
 price_increase_date = pd.to_datetime('2021-01-15')
 
 # Add a shape for the vertical line
-fig.add_shape(type='line',
+line_chart.add_shape(type='line',
               x0=price_increase_date,
               x1=price_increase_date,
               y0=sales_data['sales'].min(),
@@ -29,20 +32,26 @@ fig.add_shape(type='line',
               line=dict(color='red', dash='dash'))
 
 # Update layout for annotation
-fig.add_annotation(x=price_increase_date,
-                   y=sales_data['sales'].max() * 0.9,  # Adjust y position as needed
+line_chart.add_annotation(x=price_increase_date,
+                   y=sales_data['sales'].max() * 0.9, 
                    text='Price Increase',
                    showarrow=True,
                    arrowhead=2,
                    font=dict(size=14, weight='bold') )
 
+# Create a dcc gragh with the line chart
+visualizer = dcc.Graph(id="visualizer", figure=line_chart)
+
+# Create a header
+header = html.H2("Pink Morsel Visualizer", id="header")
 
 
 # Layout of the Dash app
 app.layout = html.Div([
-    html.H1("Soul Foods Sales Visualizer"),
-    dcc.Graph(figure=fig),
+    header,
+    visualizer
 ])
+    
 
 # Run the app
 if __name__ == '__main__':
